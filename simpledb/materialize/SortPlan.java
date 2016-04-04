40d2fd063bb87669423e08fd8c482cc020d46aab
Shizhe Chen <shizhe95@gmail.com>
2016-04-04 16:02:06 -0400

    fix the start a new run

--- simpledb/materialize/SortPlan.java
+++ simpledb/materialize/SortPlan.java
@@ -15,7 +15,7 @@ public class SortPlan implements Plan {
    private Transaction tx;
    private Schema sch;
    private RecordComparator comp;
-   
+
    /**
     * Creates a sort plan for the specified query.
     * @param p the plan for the underlying query
@@ -28,13 +28,14 @@ public class SortPlan implements Plan {
       sch = p.schema();
       comp = new RecordComparator(sortfields);
    }
-   
+
    /**
     * This method is where most of the action is.
     * Up to 2 sorted temporary tables are created,
     * and are passed into SortScan for final merging.
     * @see simpledb.query.Plan#open()
     */
+    // key method!!!
    public Scan open() {
       Scan src = p.open();
       List<TempTable> runs = splitIntoRuns(src);
@@ -43,7 +44,7 @@ public class SortPlan implements Plan {
          runs = doAMergeIteration(runs);
       return new SortScan(runs, comp);
    }
-   
+
    /**
     * Returns the number of blocks in the sorted table,
     * which is the same as it would be in a
@@ -57,7 +58,7 @@ public class SortPlan implements Plan {
       Plan mp = new MaterializePlan(p, tx); // not opened; just for analysis
       return mp.blocksAccessed();
    }
-   
+
    /**
     * Returns the number of records in the sorted table,
     * which is the same as in the underlying query.
@@ -66,7 +67,7 @@ public class SortPlan implements Plan {
    public int recordsOutput() {
       return p.recordsOutput();
    }
-   
+
    /**
     * Returns the number of distinct field values in
     * the sorted table, which is the same as in
@@ -76,7 +77,7 @@ public class SortPlan implements Plan {
    public int distinctValues(String fldname) {
       return p.distinctValues(fldname);
    }
-   
+
    /**
     * Returns the schema of the sorted table, which
     * is the same as in the underlying query.
@@ -85,27 +86,44 @@ public class SortPlan implements Plan {
    public Schema schema() {
       return sch;
    }
-   
+
    private List<TempTable> splitIntoRuns(Scan src) {
       List<TempTable> temps = new ArrayList<TempTable>();
       src.beforeFirst();
-      if (!src.next())
-         return temps;
+      if (!src.next()) {
+          return temps;
+      }
+      System.out.println("Start A New Run");
       TempTable currenttemp = new TempTable(sch, tx);
       temps.add(currenttemp);
       UpdateScan currentscan = currenttemp.open();
-      while (copy(src, currentscan))
-         if (comp.compare(src, currentscan) < 0) {
-         // start a new run
-         currentscan.close();
-         currenttemp = new TempTable(sch, tx);
-         temps.add(currenttemp);
-         currentscan = (UpdateScan) currenttemp.open();
+      while (copy(src, currentscan)) {
+          if (comp.compare(src, currentscan) < 0) {
+              // print out the old contents
+              currentscan.beforeFirst();
+              while (currentscan.next()) {
+                  System.out.println(currentscan.getVal("col1"));
+              }
+              System.out.println(" ");
+              // start a new run
+              System.out.println("Starting A New Run");
+              currentscan.close();
+              currenttemp = new TempTable(sch, tx);
+              temps.add(currenttemp);
+              currentscan = (UpdateScan) currenttemp.open();
+          }
       }
+      // print the very last run's contents
+      currentscan.beforeFirst();
+      while (currentscan.next()) {
+          System.out.println(currentscan.getVal("col1"));
+      }
+      System.out.println(" ");
       currentscan.close();
+      System.out.println("Number of Runs is " + temps.size());
       return temps;
    }
-   
+
    private List<TempTable> doAMergeIteration(List<TempTable> runs) {
       List<TempTable> result = new ArrayList<TempTable>();
       while (runs.size() > 1) {
@@ -117,13 +135,13 @@ public class SortPlan implements Plan {
          result.add(runs.get(0));
       return result;
    }
-   
+
    private TempTable mergeTwoRuns(TempTable p1, TempTable p2) {
       Scan src1 = p1.open();
       Scan src2 = p2.open();
       TempTable result = new TempTable(sch, tx);
       UpdateScan dest = result.open();
-      
+
       boolean hasmore1 = src1.next();
       boolean hasmore2 = src2.next();
       while (hasmore1 && hasmore2)
@@ -131,7 +149,7 @@ public class SortPlan implements Plan {
          hasmore1 = copy(src1, dest);
       else
          hasmore2 = copy(src2, dest);
-      
+
       if (hasmore1)
          while (hasmore1)
          hasmore1 = copy(src1, dest);
@@ -143,7 +161,7 @@ public class SortPlan implements Plan {
       dest.close();
       return result;
    }
-   
+
    private boolean copy(Scan src, UpdateScan dest) {
       dest.insert();
       for (String fldname : sch.fields())
--- simpledb/mergeTest.java
+++ simpledb/mergeTest.java
@@ -9,15 +9,15 @@ public class mergeTest {
     public static void main(String[] args) {
         SimpleDB.init("studentdb");
         Transaction tx = new Transaction();
-        Plan p1 = new TablePlan("tt", tx);
+        Plan p1 = new TablePlan("messy", tx);
 
-        System.out.println("The initial content in the messy file");
-        TableScan testScan = (TableScan) p1.open();
-        while (testScan.next()) {
-            System.out.println(testScan.getVal("col1"));
-        }
-        testScan.close();
-        System.out.println(" ");
+        // System.out.println("The initial content in the messy file");
+        // TableScan testScan = (TableScan) p1.open();
+        // while (testScan.next()) {
+        //     System.out.println(testScan.getVal("col1"));
+        // }
+        // testScan.close();
+        // System.out.println(" ");
 
         List<String> sf = Arrays.asList("col1");
         Schema sch = p1.schema();
@@ -31,6 +31,7 @@ public class mergeTest {
             return ;
         }
         System.out.println("Begin MERGE SORT Test With " + nruns + " Run Merging");
+        System.out.println(" ");
 
         // Plan p2 = new SortPlan(p1, sf, tx, nruns);
         Plan p2 = new SortPlan(p1, sf, tx);
